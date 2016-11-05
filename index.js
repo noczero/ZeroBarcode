5310006bac1ed5ad4373f638121fc1c755aba633
noczero <zeroonetm@gmail.com>
2016-11-05 15:57:31 +0700

    Error Exception Added

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var BarcodeReaders = require('node-hid-stream').KeyboardLines;
 var barcodeReaders = new BarcodeReaders({vendorId : 5050, productId : 24 });
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-	host : 'localhost',
+	host : '192.168.0.100',
 	user : 'root',
-	password : 'noczero',
+	password : '',
 	database : 'barcode'
 });
 
@@ -16,6 +16,8 @@ connection.connect(function(err) {
   // connected! (unless `err` is set)
   if (err) throw err;
   console.log('Koneksi Database Berhasil');
+  //alert();
+ wpi.digitalWrite(pin,0);
 });
 
 connection.ping(function (err) {
@@ -28,16 +30,17 @@ barcodeReaders.on("data" , function(data) {
 	console.log('===============================');
 	console.log(data);
 	console.log('Insert ke Database : ');
-	var post = {ID : data};
+	var post = {id : data};
 	insertData(post);
 	console.log('===============================');
 	panggilData();
 });
 
 function insertData(x) {
-	connection.query('INSERT INTO onlyOne SET ?' , x , function(err, result) { 
+	connection.query('INSERT INTO data_barcode SET ?' , x , function(err, result) { 
 		if(err){
 			console.log('Error : Data sudah ada');
+			alert();
 		} else {
 			console.log('Success : Data berhasil diinput');
 		}
@@ -45,7 +48,7 @@ function insertData(x) {
 }
 
 function panggilData() {
-	connection.query('SELECT * FROM onlyOne', function(err,rows){
+	connection.query('SELECT * FROM data_barcode', function(err,rows){
 	  if(err) throw err;
 	  console.log('Data ID Barcode (JSON Type):');
 	  console.log('===============================');
@@ -54,3 +57,19 @@ function panggilData() {
 };
 
 //connection.end();
+
+var wpi = require('wiring-pi');
+wpi.setup('wpi');
+var pin = 21;
+var value = 1;
+wpi.pinMode(pin , wpi.OUTPUT);
+
+function alert() {
+	//wpi.pinMode(pin , wpi.OUTPUT);
+	wpi.digitalWrite(pin,value);
+	//value = +! value;
+	wpi.delay(1000);
+	wpi.digitalWrite(pin,0);
+};
+
+//setInterval(alert , 500);
